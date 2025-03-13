ba283a2f42aa014ab842a669db98020c019d3c9b
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-03-13 08:56:06 +0000

    tutorial finished

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,13 +1,10 @@
-package main.java;
-
 /**
  * Created by abbyr on 12/03/2025
  * COMMENTS ABOUT PROGRAM HERE
  */
 public class Calculator
 {
-      public int add(int a, int b) {
-         return a + b;
-      }
-
+   public int add(int a, int b) {
+      return a + b;
+   }
 }//class
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,16 +1,16 @@
-package test.java;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by abbyr on 12/03/2025
  * COMMENTS ABOUT PROGRAM HERE
  */
-import main.java.Calculator;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-public class CalculatorTest {
+public class CalculatorTest
+{
    @Test
    void testAddition() {
       Calculator calc = new Calculator();
       assertEquals(5, calc.add(2, 3)); // Check if 2 + 3 = 5
    }
-}
+}//class
